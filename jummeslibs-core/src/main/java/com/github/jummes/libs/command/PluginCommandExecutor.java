8d11b8ac33ffe6a63d482db244db70314508f074
ZamponiMarco <zamponi.marco3@gmail.com>
2020-01-20 14:00:44 +0100

    [UPDATE] Added working TabCompleter

--- jummeslibs-core/src/main/java/com/github/jummes/libs/command/PluginCommandExecutor.java
+++ jummeslibs-core/src/main/java/com/github/jummes/libs/command/PluginCommandExecutor.java
@@ -1,22 +1,28 @@
 package com.github.jummes.libs.command;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
-public class PluginCommandExecutor implements CommandExecutor {
+public class PluginCommandExecutor implements CommandExecutor, TabCompleter {
 
 	private Class<? extends AbstractCommand> defaultCommand;
 	private Map<String, Class<? extends AbstractCommand>> commandMap;
 
-	public PluginCommandExecutor(Class<? extends AbstractCommand> defaultCommand) {
+	public PluginCommandExecutor(Class<? extends AbstractCommand> defaultCommand, String defaultCommandString) {
 		this.defaultCommand = defaultCommand;
 		commandMap = new HashMap<String, Class<? extends AbstractCommand>>();
+		commandMap.put(defaultCommandString, defaultCommand);
 	}
 
 	public void registerCommand(String name, Class<? extends AbstractCommand> commandClass) {
@@ -36,6 +42,16 @@ public class PluginCommandExecutor implements CommandExecutor {
 			e.printStackTrace();
 		}
 		return false;
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		final List<String> completions = new ArrayList<>();
+		if (args.length == 1) {
+			StringUtil.copyPartialMatches(args[0], commandMap.keySet(), completions);
+		}
+		Collections.sort(completions);
+		return completions;
 	}
 
 }
